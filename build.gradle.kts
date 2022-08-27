26e99a5b6cf7deee0bf5959b8dc57586fe38b52f
PHSSGG <pedrohddgd@gmail.com>
2022-08-27 11:09:25 -0300

    update android build tools versions to 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.android.tools.build:gradle:7.1.3")
-        classpath(kotlin("serialization", version = "1.5.31"))
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath(kotlin("serialization", version = "1.7.10"))
     }
 }
 
